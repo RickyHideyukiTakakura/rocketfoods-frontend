17b7d72b8f67511355d5b18b2f6c7b181d48a7e3
Ricky Hideyuki Takakura <rickytakakura@outlook.com>
2023-08-17 14:17:42 -0300

    initial commit

---
+++ src/styles/global.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    --webkit-font-smoothing: antialiased;
+  }
+
+  :root {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.COLORS.DARK_400};
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+  }
+
+  body, input, button, textarea {
+    font-family: "Roboto", sans-serif;
+    font-size: 1.6rem;
+    outline: none;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button, a {
+    cursor: pointer;
+    transition: filter 0.2s;
+  }
+
+  button:hover, a:hover {
+    filter: brightness(0.9);
+  }
+`;
