f67a138660c10529cd2d6e7ef163386f2d2e7991
Ricky Hideyuki Takakura <rickytakakura@outlook.com>
2023-09-25 12:49:50 -0300

    feat: possible to register a new dish

--- src/pages/NewDish/index.jsx
+++ src/pages/NewDish/index.jsx
@@ -1,20 +1,59 @@
+import { useState } from "react";
 import { PiUploadSimple } from "react-icons/pi";
 import { RiArrowLeftSLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { IngredientItem } from "../../components/IngredientItem";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { api } from "../../services/api";
 import * as S from "./styles";
 
 export function NewDish() {
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState(0.0);
+  const [description, setDescription] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [newIngredient, setNewIngredient] = useState("");
+
+  async function handleSubmitNewDish() {
+    await api.post("/dishes", {
+      name,
+      category,
+      price,
+      description,
+      ingredients,
+    });
+
+    alert("New dish created successfully");
+    navigate(-1);
+  }
+
+  function handleAddIngredient() {
+    setIngredients((prevState) => [...prevState, newIngredient]);
+    setNewIngredient("");
+  }
+
+  function handleDeleteIngredient(deletedIngredient) {
+    setIngredients((prevState) =>
+      prevState.filter((ingredient) => ingredient !== deletedIngredient)
+    );
+  }
+
+  function handleNavigateToHome() {
+    navigate(-1);
+  }
+
   return (
     <S.NewDish>
       <Header />
 
       <S.Content>
-        <button>
+        <button onClick={handleNavigateToHome}>
           {<RiArrowLeftSLine />}
           <span>Voltar</span>
         </button>
@@ -37,29 +76,55 @@ export function NewDish() {
 
           <label className="name" htmlFor="name">
             Nome
-            <Input id="name" type="text" placeholder="Ex: Salada Caesar" />
+            <Input
+              id="name"
+              type="text"
+              placeholder="Ex: Salada Caesar"
+              onChange={(event) => setName(event.target.value)}
+            />
           </label>
 
           <label className="category" htmlFor="category">
             Categoria
-            <select name="category" id="category">
-              <option>Refeição</option>
-              <option>Sobremesa</option>
-              <option>Comida</option>
+            <select
+              name="category"
+              id="category"
+              onChange={(event) => setCategory(event.target.value)}
+            >
+              <option value="refeicao">Refeição</option>
+              <option value="sobremesa">Sobremesa</option>
+              <option value="comida">Comida</option>
             </select>
           </label>
 
           <label className="ingredients" htmlFor="ingredients">
             Ingredientes
             <S.ContainerTags>
-              <IngredientItem value="React" />
-              <IngredientItem isNew placeholder="Nova tag" />
+              {ingredients.map((ingredient, index) => (
+                <IngredientItem
+                  key={String(index)}
+                  value={ingredient}
+                  onClick={() => handleDeleteIngredient(ingredient)}
+                />
+              ))}
+              <IngredientItem
+                isNew
+                placeholder="Novo ingrediente"
+                value={newIngredient}
+                onChange={(event) => setNewIngredient(event.target.value)}
+                onClick={handleAddIngredient}
+              />
             </S.ContainerTags>
           </label>
 
           <label className="price" htmlFor="price">
             Preço
-            <Input type="text" id="price" placeholder="R$00,00" />
+            <Input
+              type="text"
+              id="price"
+              placeholder="R$00,00"
+              onChange={(event) => setPrice(event.target.value)}
+            />
           </label>
 
           <label className="description" htmlFor="description">
@@ -67,10 +132,15 @@ export function NewDish() {
             <Textarea
               id="description"
               placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+              onChange={(event) => setDescription(event.target.value)}
             />
           </label>
 
-          <Button className="button" title="Salvar alterações" />
+          <Button
+            className="button"
+            title="Salvar alterações"
+            onClick={handleSubmitNewDish}
+          />
         </S.Form>
       </S.Content>
 
