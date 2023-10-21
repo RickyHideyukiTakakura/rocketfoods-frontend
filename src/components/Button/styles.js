6047d0f0e81affc898f762befd13f6ecdf522f6d
Ricky Hideyuki Takakura <rickytakakura@outlook.com>
2023-10-20 22:05:09 -0300

    fix: centralized button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Button = styled.button`
   display: flex;
   align-items: center;
+  justify-content: center;
   gap: 0.6rem;
 
   background: ${({ theme }) => theme.COLORS.TOMATO_100};
